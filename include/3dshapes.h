30aaa6f8741563e9f2af0cb3d0027239d19b96c6
Stackofsugar <digno.christopher@gmail.com>
2020-09-04 12:26:25 +0700

    Implement basic 3D headers

--- include/3dshapes.h
+++ include/3dshapes.h
@@ -3,3 +3,20 @@
 #include <iomanip>
 using namespace std;
 
+class Cube {
+    private:
+        double EDGE;
+    public:
+        Cube(double);
+        double calcVolCube();
+        double calcArCube();
+};
+
+class Sphere {
+    private:
+        double RADIUS;
+    public:
+        Sphere(double);
+        double calcVolSph();
+        double calcArSph();
+};
--- src/3dshapes.cpp
+++ src/3dshapes.cpp
@@ -1,1 +1,23 @@
 #include "../include/stdinc.h"
+#define PI 3.14159265359
+using namespace std;
+
+Cube::Cube(double x) {
+    EDGE = x;
+}
+double Cube::calcVolCube() {
+    return EDGE * EDGE * EDGE;
+}
+double Cube::calcArCube() {
+    return (EDGE * EDGE) * 6;
+}
+
+Sphere::Sphere(double x) {
+    RADIUS = x;
+}
+double Sphere::calcVolSph() {
+    return (4 * (PI * RADIUS * RADIUS * RADIUS)) / 3;
+}
+double Sphere::calcArSph() {
+    return 4 * (PI * RADIUS * RADIUS);
+}
